e26f221015c702a74145a6fdcde118b3f3475a96
olgamur <olga.mur2910@gmail.com>
2015-09-30 16:08:34 +0300

    append to last commit

--- js/Game.js
+++ js/Game.js
@@ -211,6 +211,7 @@ Game.prototype.showModal = function () {
         button_ok = document.getElementById("button-ok"),
         game = this;
     overlay.className += " active";
+    button_ok.removeAttribute("filter");
     var cell = this.buildCell(0, 0, 0, 70, "ок");
     cell.setAttribute("class", "");
     button_ok.appendChild(cell);
